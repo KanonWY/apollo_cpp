26d7cf5e6b9c42106b1906e580acdf2b440eee8e
kanonwy <2912764021@qq.com>
2023-04-14 11:21:37 +0800

    add cli help version info

--- example/sample_cli_base.cpp
+++ example/sample_cli_base.cpp
@@ -7,11 +7,22 @@ DEFINE_string(key, "", "keyName not include appid/namespace prefix ");
 DEFINE_string(appId, "", "appId want to use");
 DEFINE_string(address, "http://localhost:8070", "server address");
 DEFINE_string(token, "eafb8c5d6c45c8c11961ee60a74e8f04775596b08efaf4869807060e8af1eca5", "open api need a token");
+DEFINE_string(publishtitle, "publish", "publish title");
+DEFINE_string(publishuser, "apollo", "who publish this!");
+DEFINE_string(publishcomment, "comment", "add comment about this publish");
+
+std::string strNode(YAML::Node &node)
+{
+    std::ostringstream ss;
+    ss << node;
+    return ss.str();
+}
 
 int main(int argc, char *argv[])
 {
+    gflags::SetUsageMessage("Usage: -");
+    gflags::SetVersionString("version 1.0");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
-    gflags::SetUsageMessage("hello");
     apollo_client::apollo_ctrl_base base;
     apollo_client::MultiNsConfig config;
     if (FLAGS_appId.empty()) {
@@ -51,11 +62,31 @@ int main(int argc, char *argv[])
             std::cout << "This appid have no any keys!" << std::endl;
         }
     } else if (!strcmp(FLAGS_cmd.c_str(), "gv")) {
+        if (FLAGS_key.empty() || FLAGS_ns.empty()) {
+            std::cout << "gv command must use key and ns!" << std::endl;
+            goto fin;
+        } else {
+            auto res = base.getConfig(FLAGS_appId, FLAGS_ns, FLAGS_key);
+            if (res.has_value()) {
+                std::cout << strNode(res.value()) << std::endl;
+            } else {
+                std::cout << FLAGS_key << " is not exist!" << std::endl;
+            }
+        }
         //get specail key value
     } else if (!strcmp(FLAGS_cmd.c_str(), "del")) {
         // delete a special key
     } else if (!strcmp(FLAGS_cmd.c_str(), "pub")) {
-        // publish a key
+        if (FLAGS_publishtitle.empty()) {
+            std::cout << "Must input publishtitle" << std::endl;
+            goto fin;
+        }
+        if (base.publishNamespace(FLAGS_ns, FLAGS_publishtitle, FLAGS_publishuser, FLAGS_publishcomment)) {
+            std::cout << "publish " << FLAGS_ns << " success!" << std::endl;
+        } else {
+            std::cout << "publish " << FLAGS_ns << " error!" << std::endl;
+        }
+
     } else {
         std::cout << "unknow cmd Type" << std::endl;
     }
